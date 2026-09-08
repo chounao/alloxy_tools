b5b609ef2bec1bea96ed944a231c393fb34b20d9
chaojiezhou <zhou112517@gmail.com>
2026-09-08 15:20:18 +0800

    pat_dou

--- web_page_operation/wallet_function/transfer_out/transfer_out_operation.py
+++ web_page_operation/wallet_function/transfer_out/transfer_out_operation.py
@@ -1,7 +1,7 @@
 from common.simple_request import HttpRequest
 from common import read_and_save_tool
 from transfer_out_fee import TransferOutFee
-
+import random
 
 class TransferOutOperation:
     def __init__(self, user_http=None):
@@ -19,8 +19,18 @@ class TransferOutOperation:
         :return: 链
         """
         chain_id = self.http_request.send_request(api_name='钱包-获取支持的链列表', jsonpath_expr=f'$.data.data[?(@.chain_name=="{chain_name}")].chain_id')
-        # print(chain_id)
+
         return chain_id
+
+    def chain(self):
+        """
+        获取对应的链
+        :return: 链
+        """
+        chain = self.http_request.send_request(api_name='钱包-获取支持的链列表'
+                                               )
+        print(chain.text)
+
 
     #获取转出账户id
     def get_payee_id(self, currency,chain_id):
@@ -37,10 +47,8 @@ class TransferOutOperation:
             'chain_id': chain_id,
 
         }
-        payee_data = self.http_request.send_request(api_name='钱包-获取加密收款地址列表', dict_data=data,jsonpath_expr=f'$.data.list[?(@.status=="active")]')
-        # print(payee_data)
-        payee_id = payee_data['id']
-        # print(payee_id)
+        payee_id = self.http_request.send_request(api_name='钱包-获取加密收款地址列表', dict_data=data,jsonpath_expr=f'$.data.list[?(@.status=="active")].id')
+
         return payee_id
     #转出
 
@@ -56,12 +64,19 @@ class TransferOutOperation:
         wallet_before = self.transfer_out_fee.get_wallets_data(currency)['amount']
         wallet_id = self.transfer_out_fee.get_wallets_data(currency)['id']
         chain_id = self.get_chain(chain_name)
+        print(chain_id)
         payee_id = self.get_payee_id(currency,chain_id)
         body= {"wallet_id": wallet_id,
                 "chain_id": chain_id,
-                "payee_id": payee_id,
+                "payee_id": random.choice(payee_id),
                 "amount": amount,
-                "attachments": [],
+                "attachments": [
+                    # {"file_name": "寻汇渠道接入需求文档",
+                    # "file_suffix": "pdf",
+                    # "file_type": "application/pdf",
+                    # "file_url": "https://sandbox-hangzhou-web.s3.ap-southeast-2.amazonaws.com/929ff175-e26b-43ad-ba02-467756bff867/2026-09-08/202609081413222164920.pdf"
+                    # }
+        ],
                 "check_method": "email",
                 "access_code": "123456"}
         response = self.http_request.send_request(api_name='钱包-加密资产转出', data=body)
@@ -76,9 +91,9 @@ class TransferOutOperation:
 
 
 if __name__ == '__main__':
-    amount = 1
+    amount = 13
     currency = 'USDT'
-    chain_name = 'arbitrum'
+    chain_name = 'Arbitrum'
     transfer_out_operation = TransferOutOperation()
     #chain_id = transfer_out_operation.get_chain(chain_name)
     #transfer_out_operation.get_payee_id(currency,chain_id)
